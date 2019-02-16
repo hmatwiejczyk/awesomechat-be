7c7a87c2da22790e41dd58a9e29f7a13d7f4adc1
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-02-17 00:04:57 +0100

    fix password field on validation message

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,7 @@ exports.login = async (req, res, next) => {
       res.cookie('auth', token);
       res.status(200).json({ token: 'Bearer ' + token });
     } else {
-      res.status(401).json({ message: 'Wrong password' });
+      res.status(401).json({ password: 'Wrong password' });
     }
   } catch (err) {
     res.status(500).json({ message: 'Error occured' });
